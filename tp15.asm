fdbb6913a57f5040d689a30118ad6bdcd64c6f94
Braian Vicente <bvicente@fi.uba.ar>
2015-05-31 22:51:30 +0200

    Update tp15.asm

--- tp15.asm
+++ tp15.asm
@@ -10,7 +10,8 @@ segment datos data
 
     num         dw  65535
     dieciseis   dw  16
-    cociente    db  0
+    diez		dw	10
+	cociente    db  0
     resto       db  0
     cadena      times 10    db  '0'
                 db'$' ;para agregar el fin de string para imprimir por pantalla
@@ -22,35 +23,62 @@ segment codigo code
     mov     ax,pila     ;ss <-- dir del segmento de pila
     mov     ss,ax
 
+	
+	
+	
+binToDec:
+	mov		dx,0	   ;pongo en 0 dx para la dupla dx:ax
+	mov		ax,[num]  ;copio el nro en AX para divisiones sucesivas
+	mov		si,9	   ;'si' apunta al ultimo byte de la cadena
+
+otraDiv:
+	div		word[diez]      ;dx:ax div 10 ==> dx <- resto & ax <- cociente
+
+	add		dx,48		      ;convierto a Ascii el resto
+	mov		[cadena+si],dl	;lo pongo en la posicion anterior
+	sub		si,1		      ;posiciono SI en el caracter anterior en la cadena
+	
+	cmp		ax,[diez]	;IF    cociente < 10
+	jl		finDiv		;THEN  fin division
+	
+	mov		dx,0		;pongo en 0 DX para la dupla DX:AX
+	jmp		otraDiv
+finDiv:
+	add		ax,48
+    jmp		impStr
+binToHexa:	
     mov     dx,0        ;pongo en 0 dx para la dupla dx:ax
     mov     ax,[num]  ;copio el nro en AX para divisiones sucesivas
     mov     si,9        ;'si' apunta al ultimo byte de la cadena
 
-otraDiv:
+otraDivHexa:
     div     word[dieciseis]      ;dx:ax div 10 ==> dx <- resto & ax <- cociente
     cmp     dx,10
-    jl      esNumero
+    jl      esNumeroHexa
     add     dx,55             ;convierto a Ascii el resto
-    jmp     strNum
-esNumero:
+    jmp     strNumHexa
+esNumeroHexa:
     add     dx,48
-    jmp     strNum
-strNum:
+    jmp     strNumHexa
+strNumHexa:
     mov     [cadena+si],dl  ;lo pongo en la posicion anterior
     sub     si,1              ;posiciono SI en el caracter anterior en la cadena
     
     cmp     ax,[dieciseis]  ;IF    cociente < 16
-    jl      finDiv      ;THEN  fin division
+    jl      finDivHexa      ;THEN  fin division
     
     mov     dx,0        ;pongo en 0 DX para la dupla DX:AX
-    jmp     otraDiv
-finDiv:
+    jmp     otraDivHexa
+finDivHexa:
     cmp     ax,10
-    jl      esNum
+    jl      esNumHexa
     add     ax,55
     jmp     impStr
-esNum:
+esNumHexa:
     add     ax,48
+    jmp		impStr
+	
+	
 impStr:
     mov     [cadena+si],al
     ;imprime en pantalla el numero en base 16
